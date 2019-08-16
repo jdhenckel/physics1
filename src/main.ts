90a87825d95d3a3873a76b01cdf0cef09962eb71
John Henckel <henckel.jonathan@mayo.edu>
2019-08-16 14:39:41 -0500

    messed up stuff

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import { Utils } from './utils';
-//import { Matter } from 'matter-js';
+import { Engine, World, Render, Body, Bodies, Runner, Common, Constraint, Composites, MouseConstraint } from 'matter-js';
 
-var Matter = require('matter-js');
 
 // The window.app is a global pointer to the singleton of this class.
 
@@ -10,63 +9,60 @@ export class MainApp {
 
     boot() {
         console.log('Hello world!');
-        
 
-        // Matter.js module aliases
-        var Engine = Matter.Engine,
-        World = Matter.World,
-        Body = Matter.Body,
-        Bodies = Matter.Bodies,
-        Common = Matter.Common,
-        Constraint = Matter.Constraint,
-        Composites = Matter.Composites,
-        MouseConstraint = Matter.MouseConstraint;
-
-        // create a Matter.js engine
-        var engine = Engine.create(document.body, {
-        render: {
-        options: {
-        wireframes: true,
-        showAngleIndicator: true
-        }
-        }
-        });
+        // create engine
+        var engine = Engine.create();
 
         // gravity init
-        engine.world.gravity.x = document.getElementById("x-val").innerHTML;
-        engine.world.gravity.y = document.getElementById("y-val").innerHTML;
+        engine.world.gravity.x = +document.getElementById("x-val").innerHTML;
+        engine.world.gravity.y = +document.getElementById("y-val").innerHTML;
 
         //add a mouse-controlled constraint
         var mouseConstraint = MouseConstraint.create(engine);
         World.add(engine.world, mouseConstraint);
 
         // create a load of circle bodies
-        var stack = Composites.stack(250, 5, 12, 20, 0, 0, function(x:number, y:number, column:number, row:number) {
-        return Bodies.circle(x, y, Common.random(5, 15), { friction: .001, restitution: .1, density: 5.5 });
+        var stack = Composites.stack(250, 5, 12, 20, 0, 0, function (x: number, y: number, column: number, row: number) {
+            return Bodies.circle(x, y, random(5, 15), { friction: .001, restitution: .1, density: 5.5 });
         });
 
         // add boundaries
         var offset = 5;
         World.add(engine.world, [
-        Bodies.rectangle(400, -offset, 800 + 2 * offset, 50, { isStatic: true }),
-        Bodies.rectangle(400, 600 + offset, 800 + 2 * offset, 50, { isStatic: true }),
-        Bodies.rectangle(800 + offset, 300, 50, 600 + 2 * offset, { isStatic: true }),
-        Bodies.rectangle(-offset, 300, 50, 600 + 2 * offset, { isStatic: true })
+            Bodies.rectangle(400, -offset, 800 + 2 * offset, 50, { isStatic: true }),
+            Bodies.rectangle(400, 600 + offset, 800 + 2 * offset, 50, { isStatic: true }),
+            Bodies.rectangle(800 + offset, 300, 50, 600 + 2 * offset, { isStatic: true }),
+            Bodies.rectangle(-offset, 300, 50, 600 + 2 * offset, { isStatic: true })
         ]);
-
-        // create function to update output values & manipulate gravity
-        function gravOutputUpdate(id:string, axis:string, val:string) {
-        document.getElementById(id).innerHTML = val;
-        engine.world.gravity[axis] = val;
-        }
 
         // add all of the bodies to the world
         World.add(engine.world, stack);
 
-        // run the engine
-        Engine.run(engine);
+        var x = Common.random();
+
+        // create renderer
+        var render = Render.create({
+            element: document.body,
+            engine: engine,
+            options: {
+                showVelocity: false,
+                wireframes: false
+            }
+        });
+
+        Render.run(render);
+
+        var runner = Runner.create();
+        Runner.run(runner, engine);
     }
+
+
 }
+
+function random(a:number, b: number) {
+    return Math.floor(Math.random() * (b - a)) + a;
+}
+
 //********************************************************************
 // MAIN ENTRY POINT
 // The window will exist in a browser, but will not exist in headless mode (such as jasmine)
